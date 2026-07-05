6f5dc661e75e0748f180dc1bf6d327084b628001
crkcric <crkrishna10a@gmail.com>
2026-07-05 23:14:14 +0530

    Make ALLOWED_HOSTS environment-based

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kt7s1xayir&$_91s3=i_@aw+(7wuy16v744j-4v)u219m-k4vn'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-kt7s1xayir&$_91s3=i_@aw+(7wuy16v744j-4v)u219m-k4vn')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
